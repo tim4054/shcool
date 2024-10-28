1cc694fce0103602c216c5f89f5efdd14d6443b5
tim40 <tim4054@gmail.com>
2024-10-28 19:26:33 +0300

    version_1.6.1 SQL

--- src/main/java/ru/hogwarts/school/scripts423.sql
+++ src/main/java/ru/hogwarts/school/scripts423.sql
@@ -1,11 +1,11 @@
-1. Составить первый JOIN-запрос, чтобы получить информацию обо всех студентах
+--1. Составить первый JOIN-запрос, чтобы получить информацию обо всех студентах
 (достаточно получить только имя и возраст студента) школы Хогвартс вместе с названиями факультетов.
 
-SELECT student.name, student.age
+SELECT student.name, student.age, faculty.name
 FROM student
-INNER JOIN faculty ON student.faculty = faculty.name;
+INNER JOIN faculty ON student.faculty_id = faculty.id;
 
-2. Составить второй JOIN-запрос, чтобы получить только тех студентов, у которых есть аватарки.
+--2. Составить второй JOIN-запрос, чтобы получить только тех студентов, у которых есть аватарки.
 SELECT student.name, student.age
 FROM student
 RIGHT JOIN avatar ON student.id = avatar.id;
